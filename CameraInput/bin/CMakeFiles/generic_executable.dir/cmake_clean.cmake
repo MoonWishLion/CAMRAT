becff69b3652cb59f4bbce0824f0d8f311df4f9f
MicobyteMichael <MicobyteMichael@github.com>
2019-11-09 13:51:57 -0500

    Switched to C++/CMake

---
+++ CameraInput/bin/CMakeFiles/generic_executable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generic_executable.dir/main.cpp.o"
+  "generic_executable.pdb"
+  "generic_executable"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generic_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
