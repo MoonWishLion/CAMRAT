0f3006f980fb84d273d5f3a2d125b5e2b2684db0
MicobyteMichael <MicobyteMichael@github.com>
2019-11-09 22:55:12 -0500

    Test code for camera system

--- CameraInput/src/main.cpp
+++ CameraInput/src/main.cpp
@@ -23,28 +23,49 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
- // Read the image file
- // test of making a library here
- OpenCVImageReader reader = OpenCVImageReader();
- Mat image = reader.read("./gollum.jpg");
-
- // Check for failure
- if (image.empty())
- {
-  cout << "Could not open or find the image" << endl;
-  cin.get(); //wait for any key press
-  return -1;
- }
-
- String windowName = "The Guitar"; //Name of the window
-
- namedWindow(windowName); // Create a window
-
- imshow(windowName, image); // Show our image inside the created window.
-
- waitKey(0); // Wait for any keystroke in the window
-
- destroyWindow(windowName); //destroy the created window
-
- return 0;
+	// Read the image file
+	/*Mat image = imread("./hand.jpg");
+	
+	// Check for failure
+	if (image.empty())
+	{
+		cout << "Could not open or find the image" << endl;
+		return -1;
+	}*/
+	
+	VideoCapture cap;
+	cap.open(1, CAP_ANY);
+	
+	if(!cap.isOpened())
+	{
+		cerr << "Failed to open video stream" << endl;
+		return -1;
+	}
+	
+	String windowName = "CAMRAT Hand Detection Output"; //Name of the window
+	namedWindow(windowName); // Create a window
+	//imshow(windowName, image); // Show our image inside the created window.
+	
+	Mat image;
+	while(1)
+	{
+		cap.read(image);
+		
+		if(image.empty())
+		{
+			cerr << "Failed to read image" << endl;
+			break;
+		}
+		
+		if(waitKey(5))
+		{
+			break;
+		}
+		
+		imshow(windowName, image);
+	}
+	
+	//waitKey(0); // Wait for any keystroke in the window
+	destroyWindow(windowName); //destroy the created window
+	return 0;
 }
