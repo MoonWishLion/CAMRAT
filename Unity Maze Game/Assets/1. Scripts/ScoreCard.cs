82b67a02c7d2985928db23b232e91fd9517691bf
Mehvish Ali <MoonWishLion@users.noreply.github.com>
2019-11-10 22:17:01 -0500

    Add files via upload
    Building Assets Folder; Numerous Files Needed.

---
+++ Unity Maze Game/Assets/1. Scripts/ScoreCard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets;
+
+public class ScoreCard : MonoBehaviour
+{
+    public Text MyText;
+    private int score;
+
+    // Use this for initialization
+    void Start()
+    {
+        MyText.text = "";
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        MyText.text = score + "pts";
+
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Pickable"))
+        {
+            score = score + 1;
+        }
+
+        if (coll.CompareTag("Pickable"))
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
